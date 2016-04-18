1153205cab5ab1af7f32fe4bd9ec65a930b91d17
hezhizhen <hezhizhen.yi@gmail.com>
2016-04-18 15:33:18 +0800

    part of 1001

--- POJ/1001.cpp
+++ POJ/1001.cpp
@@ -3,6 +3,27 @@ using namespace std;
 
 int main()
 {
-	cout << "hello world." << endl;
+	char r[7];
+	int n;
+	while(cin >> r >> n)
+	{
+		// output for test
+		cout << "r is: " << r << endl;
+		cout << "n is: " << n << endl;
+		
+		int cnt = 0;// count of decimal
+		int left, right;
+
+		for(left=1;left<6&&r[left]!='.';left++)
+		{
+			for(right=5;right>=0&&r[right]=='0';right--)
+			{
+				cnt = right-left;
+			}
+		}
+
+		double R;
+
+	}
 	return 0;
 }
