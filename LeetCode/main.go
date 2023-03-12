418755a9cba69cd7b97d5fcb57a3152cc910bec5
Zhizhen He <hezhizhen.yi@gmail.com>
2023-03-12 17:17:36 +0800

    update format

--- LeetCode/main.go
+++ LeetCode/main.go
@@ -5,14 +5,14 @@ import "fmt"
 func main() {
 	// https://leetcode.com/problems/longest-substring-without-repeating-characters/
 	fmt.Println("3. Longest Substring Without Repeating Characters")
-	fmt.Println(lengthOfLongestSubstring("abcabcbb"), "(Expected: 3)")
-	fmt.Println(lengthOfLongestSubstring("abba"), "(Expected: 2)")
+	fmt.Println("\t", lengthOfLongestSubstring("abcabcbb"), "(Expected: 3)")
+	fmt.Println("\t", lengthOfLongestSubstring("abba"), "(Expected: 2)")
 	// https://leetcode.com/problems/substring-with-concatenation-of-all-words/
 	fmt.Println("30. Substring with Concatenation of All Words")
-	fmt.Println(findSubstring("barfoothefoobarman", []string{"foo", "bar"}), "(Expected: [0 9])")
+	fmt.Println("\t", findSubstring("barfoothefoobarman", []string{"foo", "bar"}), "(Expected: [0 9])")
 	// https://leetcode.com/problems/validate-binary-search-tree/
 	fmt.Println("98. Validate Binary Search Tree")
-	fmt.Println(isValidBST(&TreeNode{
+	fmt.Println("\t", isValidBST(&TreeNode{
 		Val:   2,
 		Left:  &TreeNode{Val: 1},
 		Right: &TreeNode{Val: 3},
